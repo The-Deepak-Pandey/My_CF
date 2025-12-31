aba1d3a0a73ecaea462c842d4fc707453970814f
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-12-31 14:51:49 +0530

    nice one - prefix sum

--- 1100/B_Maximum_Sum.cpp
+++ 1100/B_Maximum_Sum.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
 #define pb push_back
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
@@ -20,24 +23,80 @@ const ll INF = 1e18;
 const ll MOD = 1e9 + 7;
 
 // Common Functions
-ll gcd(ll a, ll b) { while (b) { a %= b; swap(a, b); } return a; }
+ll gcd(ll a, ll b)
+{
+    while (b)
+    {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
 ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-bool is_prime(ll n) { if (n <= 1) return false; if (n <= 3) return true; if (n % 2 == 0 || n % 3 == 0) return false; for (ll i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0) return false; } return true; }
-vector<ll> sieve(ll n) { vector<ll> primes; vector<bool> is_prime(n + 1, true); for (ll p = 2; p <= n; p++) { if (is_prime[p]) { primes.pb(p); for (ll i = p * p; i <= n; i += p) { is_prime[i] = false; } } } return primes; }
-
-
-
-
-void solve() {
-    
+bool is_prime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (ll i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+vector<ll> sieve(ll n)
+{
+    vector<ll> primes;
+    vector<bool> is_prime(n + 1, true);
+    for (ll p = 2; p <= n; p++)
+    {
+        if (is_prime[p])
+        {
+            primes.pb(p);
+            for (ll i = p * p; i <= n; i += p)
+            {
+                is_prime[i] = false;
+            }
+        }
+    }
+    return primes;
 }
 
-int main() {
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vll a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    sort(all(a));
+    vll pr(n + 1, 0);
+    for (int i = 0; i < n; ++i)
+    {
+        pr[i + 1] = pr[i] + a[i];
+    }
+    ll ans = 0;
+    for (int i = 0; i <= k; ++i)
+    {
+        ans = max(ans, pr[n - (k - i)] - pr[2 * i]);
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
     fastio;
 
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
 
