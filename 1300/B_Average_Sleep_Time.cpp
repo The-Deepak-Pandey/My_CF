edec897439f9067326d19fa36d38f9540cc5f299
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2026-09-13 13:36:49 +0530

    last q left

--- 1300/B_Average_Sleep_Time.cpp
+++ 1300/B_Average_Sleep_Time.cpp
@@ -1,45 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define pb push_back
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define fi first
-#define se second
-#define mp make_pair
-#define sz(x) ((int)(x).size())
-#define vi vector<int>
-#define pii pair<int, int>
-#define vii vector<pii>
-
-typedef long long ll;
-typedef vector<ll> vll;
-typedef vector<string> vs;
-const ll INF = 1e18;
-const ll MOD = 1e9 + 7;
-
-// Common Functions
-ll gcd(ll a, ll b) { while (b) { a %= b; swap(a, b); } return a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-bool is_prime(ll n) { if (n <= 1) return false; if (n <= 3) return true; if (n % 2 == 0 || n % 3 == 0) return false; for (ll i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0) return false; } return true; }
-vector<ll> sieve(ll n) { vector<ll> primes; vector<bool> is_prime(n + 1, true); for (ll p = 2; p <= n; p++) { if (is_prime[p]) { primes.pb(p); for (ll i = p * p; i <= n; i += p) { is_prime[i] = false; } } } return primes; }
-
-
-
-
-void solve() {
-    
-}
-
 int main() {
-    fastio;
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(n, 0);
+	long long total_sum = 0;
 
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
 
-    return 0;
+	long long sum = 0;
+	for (int i = 0; i < k; i++) {
+		sum += v[i];
+	}
+
+	total_sum = sum;
+
+	int p1 = 0, p2 = k;
+
+	while (p2 < n) {
+		sum -= v[p1];
+		sum += v[p2];
+		total_sum += sum;
+		p1++;
+		p2++;
+	}
+
+	cout << fixed << setprecision(6) << 1.0 * total_sum / (n - k + 1) << endl;
 }
