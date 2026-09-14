f2110516e49a860829e7a5a5fa9111ed71b19bfb
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2026-09-14 18:17:38 +0530

    till 15 done

---
+++ 1400/B_Fortune_Telling.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+// Common Functions
+ll gcd(ll a, ll b) { while (b) { a %= b; swap(a, b); } return a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+bool is_prime(ll n) { if (n <= 1) return false; if (n <= 3) return true; if (n % 2 == 0 || n % 3 == 0) return false; for (ll i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0) return false; } return true; }
+vector<ll> sieve(ll n) { vector<ll> primes; vector<bool> is_prime(n + 1, true); for (ll p = 2; p <= n; p++) { if (is_prime[p]) { primes.pb(p); for (ll i = p * p; i <= n; i += p) { is_prime[i] = false; } } } return primes; }
+
+
+bool help(ll ind, vector<ll> &a, ll d, ll y){
+    if(ind == a.size()) return d == y;
+
+    // do add
+    bool adding = help(ind + 1, a, d + a[ind], y);
+    if(adding) return true;
+    // do xor
+    bool xoring = help(ind + 1, a, d ^ a[ind], y);
+    if(xoring) return true;
+
+    if(adding || xoring) return true;
+
+    return false;
+}
+
+
+void solve() {
+    ll n, x, y;
+    cin >> n >> x >> y;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // if(help(0, a, x, y)){
+    //     cout << "Alice\n";
+    // } else{
+    //     cout << "Bob\n";
+    // }
+
+    ll numOdds = 0;
+    for(ll i = 0; i < n; i++) {
+        if(a[i] % 2 == 1) numOdds++;
+    }
+
+    ll aliceStartParity = x%2;
+    ll bobStartParity = 1 - aliceStartParity;
+
+    ll aliceEndParity, bobEndParity;
+    if(numOdds%2 == 0) {
+        aliceEndParity = aliceStartParity;
+        bobEndParity = bobStartParity;
+    } else {
+        aliceEndParity = 1 - aliceStartParity;
+        bobEndParity = 1 - bobStartParity;
+    }
+
+    if(y%2 == aliceEndParity) {
+        cout << "Alice\n";
+    } else {
+        cout << "Bob\n";
+    }
+}
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
